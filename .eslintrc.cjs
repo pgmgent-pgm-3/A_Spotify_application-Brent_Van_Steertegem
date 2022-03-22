3c2ff44093ea434eed77c49b4ce48fb0b22ad4a0
pgm-brentvansteertegem <brent.vansteertegem@student.arteveldehs.be>
2022-03-22 13:01:39 +0100

    Installed all node modules

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from "express";
-import "dotenv/config";
-import * as path from "path";
+import express from 'express';
+import 'dotenv/config';
+import * as path from 'path';
 
 const app = express();
 app.use(express.static('public'));
