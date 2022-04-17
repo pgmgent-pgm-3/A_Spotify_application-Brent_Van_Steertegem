67a8f5634ca73b3165da799413411497f50e92f3
pgm-brentvansteertegem <brent.vansteertegem@student.arteveldehs.be>
2022-04-17 12:06:59 +0200

    Implemented web audio

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,3 +1,34 @@
-const app = () => {};
+const app = {
+  init() {
+    this.cacheElements();
+    this.registerListeners();
+  },
 
-app();
+  cacheElements() {
+    this.$playBtn = document.querySelector(".playing_song__control--play");
+  },
+
+  registerListeners() {
+    this.$playBtn.addEventListener("click", () => {
+      if (!this.$playBtn.classList.contains("isPlaying")) {
+        this.$playBtn.classList.add("isPlaying");
+        if (!this.sound) {
+          this.playSound("/assets/audio/slipknot-psychosocial.mp3");
+        } else {
+          this.sound.volume = 0.7;
+        }
+      } else {
+        this.$playBtn.classList.remove("isPlaying");
+        this.sound.volume = 0;
+      }
+    });
+  },
+
+  playSound(soundPath) {
+    this.sound = new Audio(soundPath);
+    this.sound.volume = 0.7;
+    this.sound.play();
+  },
+};
+
+app.init();
